c1ff0772d67ea004c68cc0e1f2051968e2ea1b82
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-02-25 17:22:04 +0530

    Add comment for the test classes

--- src/test/java/LaptopStoreTest.java
+++ src/test/java/LaptopStoreTest.java
@@ -7,6 +7,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+/**
+ * This sort of unit test are not recommended. Because this unit test wrote by directly connecting with the system database.
+ * This is not a good practise and will be very harmful effects to the database.
+ * But since this is not a production environment as well as this is only for the learning purpose. So we can use this just to test the crud operations methods.
+ */
 public class LaptopStoreTest extends DatabaseTableReadHelper {
     private final LaptopStore laptopStore = new LaptopStore();
 
--- src/test/java/persistance/DatabaseManagerTest.java
+++ src/test/java/persistance/DatabaseManagerTest.java
@@ -5,6 +5,11 @@ import org.testng.annotations.Test;
 
 import java.sql.Connection;
 
+/**
+ * This sort of unit test are not recommended. Because this unit test wrote by directly connecting with the system database.
+ * This is not a good practise and will be very harmful effects to the database.
+ * But since this is not a production environment as well as this is only for the learning purpose. So we can use this just to test the crud operations methods.
+ */
 public class DatabaseManagerTest {
 
     @Test
