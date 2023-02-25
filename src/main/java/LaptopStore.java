759df08138ccf01ab3b5d7adcc96a13eb8f21251
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-02-25 17:16:00 +0530

    Minor modifications

--- src/main/java/LaptopStore.java
+++ src/main/java/LaptopStore.java
@@ -2,7 +2,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import persistance.DatabaseManager;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
