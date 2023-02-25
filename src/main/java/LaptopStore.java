1f27d0a49ee484069607e3ce5fd814564783ecdd
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-02-25 16:26:52 +0530

    Add log files

--- src/main/java/LaptopStore.java
+++ src/main/java/LaptopStore.java
@@ -1,3 +1,5 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import persistance.DatabaseManager;
 
 import java.sql.Connection;
@@ -5,18 +7,22 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class LaptopStore implements DeviceStore<Laptop>{
-
+    private Logger log = LoggerFactory.getLogger(LaptopStore.class);
     public void create(Laptop laptop) {
+        Connection connection = null;
         try {
             final String insertQuery = "INSERT INTO "+SQLCommon.LAPTOP_STORE_TABLE+" (brand_name, model_name, serial_number) VALUES (?, ?, ?)";
-            Connection connection = DatabaseManager.getInstance().getConnection();
+            connection = DatabaseManager.getInstance().getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
             preparedStatement.setString(1,laptop.getBrandName());
             preparedStatement.setString(2,laptop.getModelName());
             preparedStatement.setString(3,laptop.getSerialNumber());
             preparedStatement.execute();
         } catch (SQLException e) {
+            log.error("SQLException occurring when creating the laptop in laptop store");
             e.printStackTrace();
+        }finally {
+            closeConnection(connection,"Unable to close database connection when try with the creating a laptop");
         }
     }
 
@@ -30,5 +36,16 @@ public class LaptopStore implements DeviceStore<Laptop>{
 
     public void delete(Laptop laptop) {
         //deletion logic
+    }
+
+    private void closeConnection(Connection connection, String message){
+        try {
+            if(connection != null){
+                connection.close();
+            }
+        } catch (SQLException e) {
+            log.error(message);
+            e.printStackTrace();
+        }
     }
 }
